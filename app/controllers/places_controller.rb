6bd58aa2c62d8c8030daa05f0bfbffa3ba8f9288
Daniiar <lakiwolf.90@mail.ru>
2016-12-17 13:35:43 +0600

    #Gem paperclip

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,11 @@
+class PlacesController < ApplicationController
+  def index
+  end
+
+  def new
+    @place
+  end
+
+  private
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  root 'guests#index'
+
+  get 'images/index'
+
+  get 'categories/index'
+
+  get 'places/index'
+  get 'places/new'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
